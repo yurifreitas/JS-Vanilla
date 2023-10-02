34e0925deb9cf4c2e9f9b7f28814ae940eee76ef
Thayza de Oliveira <thayzaoliveira4@gmail.com>
2023-10-02 18:45:57 -0300

    Reestrutura header e estiliza título

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,43 @@
-import { navigationRoutes } from '../scripts/routes.js';
-
 export const renderHeader = () => {
-    // Renderizar dinamicamente os links do menu
-    const navLinks = navigationRoutes.map(route => {
-        return `<a href="#${route.path}">${route.name}</a>`;
-    }).join(' ');
+  // Renderizar dinamicamente os links do menu
+  //   const navLinks = navigationRoutes
+  //     .map((route) => {
+  //       return `<a href="#${route.path}">${route.name}</a>`
+  //     })
+  //     .join(' ')
 
-    const header = document.querySelector('#header');
-    header.innerHTML = `
-        <header>
-            <div class="hamburger-menu" id="hamburger-menu">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </div>
-            <nav class="navbar" id="navbar">
-                ${navLinks}
+  const header = document.querySelector('#header')
+
+  header.innerHTML = `
+        <div>
+            <h1> Valorant info</h1>
+
+            <nav id="navigation">
+                <ul>
+                    <li></li>
+                    <li><a>Sobre o Jogo</a></li>
+                </ul>
             </nav>
-        </header>
-    `;
 
-    // Adicionar eventListeners para o menu hambúrguer
-    document.getElementById('hamburger-menu').addEventListener('click', toggleHamburgerMenu);
+            <input id="search" placeholder="Busque um personagem" maxlength="20"/>
+
+        </div>
+
+    `
+
+  const a = document.createElement('a')
+  const li = document.querySelector('li')
+  a.text = 'Personagens'
+
+  li.appendChild(a)
+
+  a.addEventListener('click', () => {
+    openSideBar()
+  })
 }
-function toggleHamburgerMenu() {
-    const navbar = document.getElementById('navbar');
-    if (navbar.style.display === "none" || !navbar.style.display) {
-        navbar.style.display = "block";
-    } else {
-        navbar.style.display = "none";
-    }
+
+export function openSideBar() {
+  const sideBar = document.querySelector('sidebar')
+  sideBar.style.display = 'block'
+  console.log('sidebar tá on!')
 }
