9dcd1015cf42142294f05de935f1c258e93e823c
Matheus Holanda <matheusholanda0997@gmail.com>
2023-10-04 14:03:45 -0300

    Atualização do footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,28 +2,44 @@ export function renderFooter() {
 
     const footer = document.querySelector('#footer');
 
-    footer.innerHTML = `
-           <section class="section-footer" id="section-texts">
-                    <a href="https://playvalorant.com/pt-br/download/" target="_blank">Baixe o jogo</a>
-                    <a href="https://assets.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt96bcb1f792b51a6b/6116afd61da1da13cddbee74/VALORANT_Community_Competition_Guidelines_por-BR_v1.2.pdf" target="_blank">Torneios Independentes</a>                    
-           </section>
-
+    footer.innerHTML = `      
            <section id="section-logos">
                     <a href="https://www.riotgames.com/pt-br?_ga=2.202650514.243762150.1696266792-99981719.1696266790" target="_blank"><img src="assets/logo-riot.png"></a>
                     <a href="https://playvalorant.com/pt-br/" target="_blank"><img src="assets/icons8-valorant-250.png"></a>              
            </section>
 
-           <section id="direitos">
-                    <p>© 2023 Riot Games, Inc. Riot Games, VALORANT e Qualquer Logotipo associado são marcas comerciais, marcas de serviço e/ou marcas registradas da Riot Games, Inc.</p>
+           <section id="project">
+                    <p>Projeto realizado durante o programa Santander Coders 2023 em parceria com ADA Tech.</p>
+                    <p>A ideia do projeto consiste em uma SPA consumindo API, utilizando rotas para navegação entre pÁginas.</p>                    
            </section>
 
-           <section id="section-links">
-                    <a href="https://www.riotgames.com/pt-br/privacy-notice-BR?_ga=2.202062098.243762150.1696266792-99981719.1696266790" target="_blank">Políticas de Privacidade</a>
-                    <a href="https://www.riotgames.com/pt-br/terms-of-service-BR?_ga=2.202062098.243762150.1696266792-99981719.1696266790" target="_blank">Termos de Serviços</a>           
-           </section>
+           <section id="container-collaborators">
+           Colaboradores  
+            <section id="contact-collaborators">
+                <section id="section-collaborators">      
+                   <p>Jamyle Layla</p> 
+                   <a href="https://www.linkedin.com/in/jamyle-teles-151259235/" target="_blank"><img src="assets/linkedin-logo-50.png"</a>
+                   <a href="https://github.com/JamyleTeles" target="_blank"><img src="assets/github-logo.png"</a>
+                </section>
 
-           <section id="section-classificacao">
-                    <img id="classificacao" src="https://images.contentstack.io/v3/assets/blt0eb2a2986b796d29/blt84ae391429f2375a/5f2db8f41967337e7590c9a8/riot-seloclassind-14-val.png?&height=100&disable=upscale">
-           </section>
+                <section id="section-collaborators">      
+                   <p>Juan Salles</p> 
+                   <a href="https://www.linkedin.com/in/juan-salles/" target="_blank"><img src="assets/linkedin-logo-50.png"</a>
+                   <a href="https://github.com/JuanSalles" target="_blank"><img src="assets/github-logo.png"</a>
+                </section>
+
+                <section id="section-collaborators">      
+                   <p>Matheus Holanda</p> 
+                   <a href="https://www.linkedin.com/in/matheus-holanda97/" target="_blank"><img src="assets/linkedin-logo-50.png"</a>
+                   <a href="https://github.com/MMatheus-Holanda" target="_blank"><img src="assets/github-logo.png"</a>
+                </section>
+
+                <section id="section-collaborators">      
+                   <p>Thayza Oliveira</p>  
+                   <a href="https://www.linkedin.com/in/tthayza-oliveira/" target="_blank"><img src="assets/linkedin-logo-50.png"</a>
+                   <a href="https://github.com/tthayza" target="_blank"><img src="assets/github-logo.png"</a>
+                </section>
+             </section>           
+           </section>         
     `;
 }
